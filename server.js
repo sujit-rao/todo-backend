b9634704fea3373191a514b5f750afb12c44fe41
Sujit Rao <sujitrao867@gmail.com>
2024-04-19 22:42:13 +0530

    cors again

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://todo-frontend-gamma-three.vercel.app'
+    origin: 'https://todo-frontend-gamma-three.vercel.app/'
   }));
 
 
